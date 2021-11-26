edd4d5f1d57baebda09186fe4e07db0b54cd176c
Alex-Zicaro <alex.zicaro@laplateforme.io>
2021-11-26 14:44:46 +0100

    bddmodif

--- include/bdd.php
+++ include/bdd.php
@@ -1,6 +1,7 @@
 <?php
     try {
-        $bdd = new PDO('mysql:host=localhost;dbname=alex-zicaro_moduleconnexion;charset=utf8', 'alex-zicaro', 'Lilinette83', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $bdd = new PDO('mysql:host=localhost;dbname=alex-zicaro_moduleconnexion;charset=utf8', 'alex-zicaro', 'Lilinette83');
+        $bdd ->setAttribute(PDO::ATTR_ERRMODE ,PDO::ERRMODE_WARNING);
         } 
     catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
